9d8b993de0882d45b4326027999663979fe4a386
Galikhan Khamitov <galikhan.khamitov@gmail.com>
2026-01-29 11:59:04 +0500

    add serdeable

--- src/main/java/kz/medical/call/center/api/record/report/AppealAmountByOperator.java
+++ src/main/java/kz/medical/call/center/api/record/report/AppealAmountByOperator.java
@@ -2,6 +2,9 @@ package kz.medical.call.center.api.record.report;
 
 import org.jooq.Record5;
 
+import io.micronaut.serde.annotation.Serdeable;
+
+@Serdeable
 public record AppealAmountByOperator(String fullname, String username, Integer complaint,
                                      Integer proposal, Integer thanks, Integer total) {
     public static AppealAmountByOperator to(Record5<String, Integer, Integer, Integer, Integer> r) {
